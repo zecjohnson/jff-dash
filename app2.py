4ea15b696eab02e90add37638f95af097eb43d16
zecjohnson <74927653+zecjohnson@users.noreply.github.com>
2023-08-07 13:33:25 +0100

    Update app2.py

--- app2.py
+++ app2.py
@@ -4,8 +4,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import numpy as np
 import joblib
-from google.cloud import bigquery
-import db_dtypes
 import time
 
 # import dash & bootstrap libraries
